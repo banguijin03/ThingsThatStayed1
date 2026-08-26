97932e07bb2c2d74e3acdb7346750d9670c55c02
dreamzoe <dreamzoe04@gmail.com>
2026-08-26 16:54:18 +0900

    npc상호작용 모듈

--- Assets/0.Scripts/Objects/Characters/CharacterModules/NPC/NPCDialogueModule.cs
+++ Assets/0.Scripts/Objects/Characters/CharacterModules/NPC/NPCDialogueModule.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
 
-public class NPCDialogueModule : MonoBehaviour
+public class NPCDialogueModule : CharacterModule
 {
     public void Dialoogue()
     {
-
+        Debug.Log("대화중이에용");
+        Debug.Log("대화를 끝낼거에요");
     }
 }
--- Assets/0.Scripts/Objects/Characters/CharacterModules/NPC/NPCInteractionModule.cs
+++ Assets/0.Scripts/Objects/Characters/CharacterModules/NPC/NPCInteractionModule.cs
@@ -1,19 +1,64 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 public class NPCInteractionModule : NPCModule
 {
-    bool able = true;
+    [SerializeField] PlayerController playerCharacter;
     NPCDialogueModule Dialogue;
-    public void PlayerInteration()
+
+    public override void OnRegistration(CharacterBase newOwner)
     {
-        InputManager.OnInteraction -=  Interactionable;
-        InputManager.OnInteraction +=  Interactionable;
-        CommandStart();
+        base.OnRegistration(newOwner);
+        playerCharacter= FindAnyObjectByType<PlayerController>(FindObjectsInactive.Include);
+        InputManager.OnInteraction -= Interactionable;
+        InputManager.OnInteraction += Interactionable;
+    }
+
+    public override void OnUnregistration(CharacterBase oldOwner)
+    {
+        InputManager.OnInteraction -= Interactionable;
+        base.OnUnregistration(oldOwner);
     }
 
     public void Interactionable(bool value)
     {
-        if (!value) return;
+        Debug.Log("함수는 실행됐어용");
+        if (!value)
+        {
+            Debug.Log("입력을 안하셨는데용?");
+            return;
+        }
+        if (playerCharacter == null)
+        {
+            Debug.Log("캐릭터가 없는데용?");
+            return;
+        }
+        if (!Owner)
+        {
+            Debug.Log("주인장이 없는데용?");
+            return;
+        }
+
+        Vector2 npcPosition = Owner.transform.position;
+        Vector2 playerPosition = playerCharacter.Character.transform.position;
+        float distance = Vector2.Distance(npcPosition, playerPosition);
+
+        if (distance > 5)
+        {
+            Debug.Log("너무멀어");
+            return;
+        }
+
+        //플레이어가 npc를 바라보고 있는 방향이 옳은지 체크
+        Vector2 directionToNPC = (npcPosition - playerPosition).normalized;
+        Vector2 playerDirection = playerCharacter.Character.LookRotation.normalized;
+        float dot = Vector2.Dot(playerDirection, directionToNPC);
+        if (dot > 0.7)
+        {
+            Debug.Log("성공!");
+            CommandStart();
+        }
     }
 
     public void CommandStart()
